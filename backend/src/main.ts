50eeed289fe5d6cc396ce2b097a1f3a10c0f1bad
dcreux <d.creux@groupe-burus.tech>
2024-07-11 10:11:50 +0200

    erreur build correction

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:4200', // Autorisez votre application Angular
@@ -12,5 +10,3 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
-
-
